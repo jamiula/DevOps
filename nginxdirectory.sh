3f5a85b7eb24f3a01cb629c7f9c371ab4fd13cb1
Jamiul <jamiulaislam@gmail.com>
2021-03-05 10:06:01 +0000

    Create Virtual Host for nginx

--- nginxdirectory.sh
+++ nginxdirectory.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
-sudo mkdir /var/www/nginx
-sudo chown -R $USER:$USER /var/www/nginx
-sudo chmod -R 755 /var/www/nginx
+sudo mkdir /var/www/nginx.api.net.bd
+sudo chown -R $USER:$USER /var/www/nginx.api.net.bd
+sudo chmod -R 755 /var/www/nginx.api.net.bd
+cat> /etc/nginx/sites-available/nginx.api.nt.bd.conf <<EOF
+server {
+        listen 80;
+        listen [::]:80;
+
+        root /var/www/nginx.api.net.bd;
+        index index.html index.htm index.nginx-debian.html;
+
+        server_name naginx.api.net.bd www.nginx.api.net.bd;
+
+        location / {
+                try_files $uri $uri/ =404;
+        }
+}
+EOF
+sudo ln -s /etc/nginx/sites-available/nginx.api.net.bd.conf /etc/nginx/sites-enabled/
 
 
